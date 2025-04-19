358213f13cace189f9395802e3f18aab2202660b
Marek Michalik <marekmichalik43@gmail.com>
2025-04-19 18:44:57 +0200

    Make template more agnostic

---
+++ init.sql
@@ -0,0 +1,4 @@
+create table tasks (
+    id serial,
+    task text
+);
--- todo.sql
+++
@@ -1,9 +0,0 @@
-create table tasks (
-    id serial,
-    task text
-);
-
-insert into tasks (task) values ('Install Pico CSS');
-insert into tasks (task) values ('Install htmx');
-insert into tasks (task) values ('Install Flask');
-insert into tasks (task) values ('Install PostgreSQL');
