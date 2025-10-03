93fc6d0e6ecec473ceddd39b077a46ba7db549e6
Marek Michalik <marekmichalik43@gmail.com>
2025-10-03 23:58:06 +0200

    Use dict rows

--- db.py
+++ db.py
@@ -5,17 +5,18 @@ import psycopg
 
 def get_db():
     db = psycopg.Connection.connect(
-        host=os.getenv('DB_HOST'),
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        dbname=os.getenv('DB_NAME'),
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        dbname=os.getenv("DB_NAME"),
     )
     db.autocommit = True
+    db.row_factory = psycopg.rows.dict_row
     return db
 
 
 def get_users(db):
-    return [row[0] for row in db.execute("select name from users order by id")]
+    return [row["name"] for row in db.execute("select name from users order by id")]
 
 
 def add_user(db, name):
